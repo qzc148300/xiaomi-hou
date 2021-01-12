7572c96731cdbe0a3b158a2cefed22ae2e0e5665
qzc <1483003917@qq.com>
2021-01-12 10:01:21 +0800

    Initial commit

---
+++ src/main/java/com/jk/XiaomiHouApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomiHouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomiHouApplication.class, args);
+    }
+
+}
